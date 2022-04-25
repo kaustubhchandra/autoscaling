02e500e0ecb01e1048be3e44554af2939539bb86
kaustubhchandra zarkar <73283594+kaustubhchandra@users.noreply.github.com>
2022-04-25 16:42:43 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -66,3 +66,22 @@ resource "aws_autoscaling_policy" "custum-cpu-policy" {
   policy_type            = "SimpleScaling"
   
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "custum-cpu-alarm" {
+  alarm_name          = "custom-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.kk-auto.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.custum-cpu-alarm.arn]
+}
